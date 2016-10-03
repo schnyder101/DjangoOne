66722bf117a10e48858d8c36ead5d59f427691b9
Suharsh Sanjay <suharshs@pune.smartek21.st21>
2016-10-03 19:10:06 +0530

    updates 2

--- trydjango/trydjango/settings.py
+++ trydjango/trydjango/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #third party apps:
+    'crispy_forms',
+    #my apps:
     'newsletter',
 ]
 
@@ -127,3 +130,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "newsletter","static"),
+    #'/var/www/static/',
+]
+
+# MEDIA_URL =
+# MEDIA_ROOT =
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
--- trydjango/trydjango/urls.py
+++ trydjango/trydjango/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from newsletter.views import home,contact
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'^$', home,name="home"),
 	url(r'^contact/$', contact,name="contact")
-]
+] 
+
+#Ensure static setting only in development mode not in production
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+   #urlpattern+=static(settings.MEDIA document_root=settings.MEDIA_ROOT)
